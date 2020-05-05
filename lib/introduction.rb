c66059b0e0f475536e6663d8fc449ba80978a3c0
Luis Gonzalez <luisagonzalez1992@icloud.com>
2020-05-05 03:15:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,5 @@ end
 
 
 def introduction_with_language(name,lauguage)
-  puts "Hi, my name is #{name} and i am learning to program in #{lauguage}."
+  puts "Hi, my name is #{name} and I am learning to program in #{lauguage}."
 end
